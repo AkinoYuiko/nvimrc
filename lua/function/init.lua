9e51c1737958dcad99ef8aa679e9dfa2d6257873
Civi <civi@rcg.fun>
2026-01-14 12:28:54 +0800

    refactor: move plugin setup out of function init

--- lua/function/init.lua
+++ lua/function/init.lua
@@ -23,31 +23,6 @@ local function package_deffered_fn()
 		nargs = '?',
 		complete = function() return { 'silent' } end,
 	})
-	-- Mini Packs Setup
-	local mini_modules = {
-		-- 'ai',
-		['completion'] = {},
-		['cmdline'] = {},
-		['files'] = { windows = { preview = true } },
-		['icons'] = {},
-		['notify'] = {},
-		['pick'] = {},
-		['snippets'] = {},
-		['statusline'] = {},
-		['tabline'] = {},
-	}
-	for mod, opts in pairs(mini_modules) do
-		require('mini.' .. mod).setup(opts)
-	end
-	local ok, mini_keymap = pcall(require, 'mini.keymap')
-	if ok then
-		local map_multistep = mini_keymap.map_multistep
-		if map_multistep ~= nil then
-			map_multistep('i', '<Tab>', { 'pmenu_next' })
-			map_multistep('i', '<S-Tab>', { 'pmenu_prev' })
-			map_multistep('i', '<CR>', { 'pmenu_accept', 'minipairs_cr' })
-		end
-	end
 	-- keymap
 	require('keymap')
 	-- Treesitter
@@ -60,13 +35,6 @@ au('BufEnter', {
 	once = true,
 	callback = function()
 		require('plugin.everforest')
-		-- Colorscheme
-		require('everforest').setup({
-			background = 'hard',
-			transparent_background_level = 2,
-			show_eob = false,
-		})
-		vim.cmd.colorscheme('everforest')
 		vim.defer_fn(package_deffered_fn, 0)
 		-- vim.defer_fn(function() end, 0)
 	end,
--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -1,11 +1,11 @@
 -- Packages --
 vim.pack.add({
-	{ src = 'https://github.com/neanias/everforest-nvim' },
+	-- { src = 'https://github.com/neanias/everforest-nvim' },
 	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
 	{ src = 'https://github.com/neovim/nvim-lspconfig' },
 	{ src = 'https://github.com/mason-org/mason.nvim' },
 	{ src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
-	{ src = 'https://github.com/nvim-mini/mini.ai' },
+	-- { src = 'https://github.com/nvim-mini/mini.ai' },
 	{ src = 'https://github.com/nvim-mini/mini.completion' },
 	{ src = 'https://github.com/nvim-mini/mini.cmdline' },
 	{ src = 'https://github.com/nvim-mini/mini.files' },
@@ -19,3 +19,29 @@ vim.pack.add({
 	-- { src = "https://github.com/farmergreg/vim-lastplace" },
 	-- { src = "https://github.com/Darazaki/indent-o-matic" },
 }, { confirm = false })
+
+-- Mini Packs Setup
+local mini_modules = {
+	-- 'ai',
+	['completion'] = {},
+	['cmdline'] = {},
+	['files'] = { windows = { preview = true } },
+	['icons'] = {},
+	['notify'] = {},
+	['pick'] = {},
+	['snippets'] = {},
+	['statusline'] = {},
+	['tabline'] = {},
+}
+for mod, opts in pairs(mini_modules) do
+	require('mini.' .. mod).setup(opts)
+end
+local ok, mini_keymap = pcall(require, 'mini.keymap')
+if ok then
+	local map_multistep = mini_keymap.map_multistep
+	if map_multistep ~= nil then
+		map_multistep('i', '<Tab>', { 'pmenu_next' })
+		map_multistep('i', '<S-Tab>', { 'pmenu_prev' })
+		map_multistep('i', '<CR>', { 'pmenu_accept', 'minipairs_cr' })
+	end
+end
