1fde870044abf78fc5f752728a1233b515683db1
Civi <civi@rcg.fun>
2026-01-15 12:28:47 +0800

    feat: use sainnhe/everforest instead
    This will add 6ms startup time
    will optimize in the future

--- lua/plugin/everforest.lua
+++ lua/plugin/everforest.lua
@@ -1,10 +1,12 @@
 vim.pack.add({
-	{ src = 'https://github.com/neanias/everforest-nvim' },
+	{ src = 'https://github.com/sainnhe/everforest' },
 }, { confirm = false })
 -- Colorscheme settings
-require('everforest').setup({
-	background = 'hard',
-	transparent_background_level = 2,
-	show_eob = false,
-})
+-- require('everforest').setup({
+-- 	background = 'hard',
+-- 	transparent_background_level = 2,
+-- 	show_eob = false,
+-- })
+vim.g.everforest_background = "hard"
+vim.g.everforest_transparent_background = 2
 vim.cmd.colorscheme('everforest')
