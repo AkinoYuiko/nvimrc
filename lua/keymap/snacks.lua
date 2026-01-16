8804cb9cf009a8c2e4af45d966ab32f075f81ef4
Civi <civi@rcg.fun>
2026-01-16 19:29:50 +0800

    feat: tweak snacks keymap

--- lua/keymap/snacks.lua
+++ lua/keymap/snacks.lua
@@ -5,7 +5,7 @@ local keys = {
 	{ '<leader>/', function() Snacks.picker.grep() end, desc = 'Grep' },
 	{ '<leader>:', function() Snacks.picker.command_history() end, desc = 'Command History' },
 	-- { '<leader>n', function() Snacks.picker.notifications() end, desc = 'Notification History' },
-	-- { '<leader>e', function() Snacks.explorer() end, desc = 'File Explorer' },
+	{ '<leader>e', function() Snacks.explorer() end, desc = 'File Explorer' },
 	-- find
 	{ '<leader>f', function() Snacks.picker.files() end, desc = 'Find Files' },
 	-- { '<leader>fb', function() Snacks.picker.buffers() end, desc = 'Buffers' },
@@ -30,7 +30,7 @@ local keys = {
 	-- Grep
 	-- { '<leader>sb', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
 	-- { '<leader>sB', function() Snacks.picker.grep_buffers() end, desc = 'Grep Open Buffers' },
-	{ '<leader>sg', function() Snacks.picker.grep() end, desc = 'Grep' },
+	-- { '<leader>sg', function() Snacks.picker.grep() end, desc = 'Grep' },
 	-- { '<leader>sw', function() Snacks.picker.grep_word() end, desc = 'Visual selection or word', mode = { 'n', 'x' } },
 	-- search
 	-- { '<leader>s"', function() Snacks.picker.registers() end, desc = 'Registers' },
