4906738f6db4d6a4a3bb14c7f47959272449e1dc
Civi <civi@rcg.fun>
2026-01-17 01:34:58 +0800

    refactor: use another gitui plugin

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -2,7 +2,7 @@
 vim.pack.add({
 	'https://github.com/nvim-treesitter/nvim-treesitter',
 	'https://github.com/neovim/nvim-lspconfig',
-	'https://github.com/aspeddro/gitui.nvim',
+	'https://github.com/mikinovation/nvim-gitui',
 	-- 'https://github.com/mason-org/mason.nvim',
 	-- 'https://github.com/mason-org/mason-lspconfig.nvim',
 	-- 'https://github.com/j-hui/fidget.nvim',
@@ -26,4 +26,4 @@ vim.pack.add({
 require('plugin.mini')
 -- Snacks
 -- require('plugin.snacks')
-require('gitui').setup()
+require('nvim-gitui').setup({ keymaps = { n = '<leader>G' } })
