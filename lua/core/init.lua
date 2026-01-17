873d2e52a885cd5b6ffa7bffdcfad2431ca9046e
Civi <civi@rcg.fun>
2026-01-17 14:31:16 +0800

    feat: disable providers that is no need
    - perl
    - ruby

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,7 +1,10 @@
 vim.g.mapleader = ' '
+-- diable for no need
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
 -- experimental feat: ext_ui
 require('vim._extui').enable({})
 require('core.options')
-
+-- load colorscheme and snacks
 require('plugin.everforest')
 require('plugin.snacks')
