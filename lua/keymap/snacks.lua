f4f58b467d6d44a716f88afca36229b7a02d3752
Civi <civi@rcg.fun>
2026-01-19 16:47:10 +0800

    refactor: disable some snacks keymap

--- lua/keymap/snacks.lua
+++ lua/keymap/snacks.lua
@@ -1,16 +1,16 @@
 require('core.keymap').map({
 	-- Top Pickers & Explorer
 	{ '<leader><space>', function() Snacks.picker.smart() end, desc = 'Smart Find Files' },
-	{ '<leader>b', function() Snacks.picker.buffers() end, desc = 'Buffers' },
-	{ '<leader>/', function() Snacks.picker.grep() end, desc = 'Grep' },
-	{ '<leader>:', function() Snacks.picker.command_history() end, desc = 'Command History' },
+	-- { '<leader>b', function() Snacks.picker.buffers() end, desc = 'Buffers' },
+	-- { '<leader>/', function() Snacks.picker.grep() end, desc = 'Grep' },
+	-- { '<leader>:', function() Snacks.picker.command_history() end, desc = 'Command History' },
 	-- { '<leader>n', function() Snacks.picker.notifications() end, desc = 'Notification History' },
 	-- explorer
-	{ '<leader>e', function() Snacks.explorer() end, desc = 'File Explorer' },
+	-- { '<leader>e', function() Snacks.explorer() end, desc = 'File Explorer' },
 	-- help
-	{ '<leader>h', function() Snacks.picker.help() end, desc = 'Help Pages' },
+	-- { '<leader>h', function() Snacks.picker.help() end, desc = 'Help Pages' },
 	-- find
-	{ '<leader>f', function() Snacks.picker.files() end, desc = 'Find Files' },
+	-- { '<leader>f', function() Snacks.picker.files() end, desc = 'Find Files' },
 	-- { '<leader>fb', function() Snacks.picker.buffers() end, desc = 'Buffers' },
 	-- { '<leader>fc', function() Snacks.picker.files({ cwd = vim.fn.stdpath('config') }) end, desc = 'Find Config File' },
 	-- { '<leader>ff', function() Snacks.picker.files() end, desc = 'Find Files' },
