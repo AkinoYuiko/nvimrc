51b2b13b66cd060e4ffb27593cd44e740dfa15be
Civi <civi@rcg.fun>
2026-01-19 17:26:17 +0800

    feat: re-enable fidget for lsp notify

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -2,24 +2,21 @@
 vim.pack.add({
 	'https://github.com/nvim-treesitter/nvim-treesitter',
 	'https://github.com/neovim/nvim-lspconfig',
-	-- 'https://github.com/mikinovation/nvim-gitui',
 	'https://github.com/mason-org/mason.nvim',
 	'https://github.com/mason-org/mason-lspconfig.nvim',
-	-- 'https://github.com/j-hui/fidget.nvim',
-	-- 'https://github.com/Darazaki/indent-o-matic',
+	'https://github.com/j-hui/fidget.nvim',
 	'https://github.com/folke/which-key.nvim',
 }, { confirm = false })
 -- mason
 require('mason').setup()
 require('mason-lspconfig').setup()
 -- fidget
--- require('fidget').setup({
--- 	notification = {
--- 		override_vim_notify = true,
--- 	},
--- })
--- indent-o-matic
--- require('indent-o-matic').setup({})
+require('fidget').setup({
+	notification = {
+		override_vim_notify = true,
+	},
+})
 -- mini.packs
 require('plugin.mini')
+-- which-key
 require('which-key').setup({ preset = 'modern' })
